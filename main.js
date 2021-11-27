810251d1f38b9805d4ed15b32881d087b27778e2
Jair Araujo <jair.jay@hotmail.com>
2021-11-27 20:35:39 -0300

    Adicionado o arquivo JS e feito um Fetch da API do Github para preencher os dados no HTML

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: "jair-araujo",
+    youtube: "rocketseat",
+    instagram: "jair_araujo1",
+    facebook: "jair.araujo.129",
+    twitter: "Jair_Araujo1",
+  };
+
+  function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute("class");
+      li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}/`;
+    }
+  }
+
+  changeSocialMediaLinks();
+
+  function getGitHubProfileInfos(){
+      const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name,
+        userBio.textContent = data.bio,
+        userLink.href = data.html_url,
+        userImage.src = data.avatar_url,
+        userLogin.textContent = data.login
+      })
+  }
+
+  getGitHubProfileInfos();
